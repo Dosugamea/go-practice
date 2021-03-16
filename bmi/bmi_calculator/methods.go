cdc8d27ad242bbeda675c1cd0ac572737ece2fa7
Dosugamea <17107514+Dosugamea@users.noreply.github.com>
2021-03-16 23:23:50 +0900

    ✨ Add too small value validation

--- bmi/bmi_calculator/methods.go
+++ bmi/bmi_calculator/methods.go
@@ -4,11 +4,17 @@ func GetBmiFromHeightAndWeight(height Height, weight Weight) (Bmi, error) {
 	if weight < 0 || height < 0 {
 		return 0, BmiError{BmiMinusValueError}
 	}
-	if weight < 20 {
+	if weight <= 20 {
 		return 0, BmiError{BmiTooSmallValueError}
 	}
-	if height < 100 {
+	if weight >= 300 {
+		return 0, BmiError{BmiTooBigValueError}
+	}
+	if height <= 100 {
 		return 0, BmiError{BmiTooSmallValueError}
+	}
+	if height >= 300 {
+		return 0, BmiError{BmiTooBigValueError}
 	}
 	var res Bmi = Bmi(
 		float64(weight) / (float64(height)/100 + float64(height)/100),
